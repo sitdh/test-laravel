1a4e4aef0e79179e64357a25f616e70ceee940a9
Sitdhibong Laokok <sitdhibong@gmail.com>
2018-02-02 10:59:48 +0700

    Define route for posts

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/posts', 'PostController@index');
+
 Route::get('/sitdh', 'BookController@index');
 Route::post('/sitdh', 'BookController@store');
 Route::get('/sitdh/{book}', 'BookController@view');
